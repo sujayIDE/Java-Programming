14b45b0e0e0fbdeecb75d9ae2eda85870a7bf4e7
Sujay Arjun Chimane <156955269+sujayIDE@users.noreply.github.com>
2026-07-11 13:31:56 +0530

    Add files via upload

---
+++ Data Types/src/Test.java
@@ -0,0 +1,22 @@
+public class Test {
+    public static void main(String[] args) {
+        //int x=10000000000; //error
+        long x= 10000000000L;
+
+       // byte b=128;     //error
+        byte b=127;
+
+        float c=4.789789789789F;
+        System.out.println(c);
+
+        double d=4.789789789789F;
+        System.out.println(d);
+
+        char j= '\u0041'; // unicode
+        System.out.println(j);
+
+        char ch=65;
+        System.out.println(ch);
+
+    }
+}
