e3727fdbdb3de4491ec542058f297f1960773f42
Sujay Arjun Chimane <156955269+sujayIDE@users.noreply.github.com>
2026-07-12 14:51:59 +0530

    Add files via upload

---
+++ TypeCasting/src/ExplicitDemo.java
@@ -0,0 +1,18 @@
+public class ExplicitDemo {
+    public static void main(String[] args) {
+        // long to int
+        long l= 10000000000000L;
+        int i=(int) l;
+        System.out.println(i);
+
+        // double to int
+        double d=1.23232323232323D;
+        int i1=(int) d;
+        System.out.println(i1);
+
+        //int to char
+        int b=931;
+        char c=(char)b;
+        System.out.println(c);
+    }
+}
---
+++ TypeCasting/src/ImplicitDemo.java
@@ -0,0 +1,20 @@
+public class ImplicitDemo {
+    public static void main(String[] args) {
+        //byte to int
+      byte b=124;
+      int x=b;
+        System.out.println("byte to int conversion:" +x);
+
+        //char to int
+        char c='A';
+        int a=c;
+        System.out.println("char to int:"+a);
+
+        //float to double
+        float f=1.243434224F;
+        double d=f;
+        System.out.println("float to double:"+d);
+
+
+    }
+}
