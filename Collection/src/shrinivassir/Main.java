089e1e7e33eb402b76d2be19c9464548e49e29fb
sujayIDE <sujaychimane6909@gmail.com>
2026-09-07 18:30:56 +0530

    pattern

--- Collection/src/shrinivassir/Main.java
+++ Collection/src/shrinivassir/Main.java
@@ -1,4 +1,20 @@
 package shrinivassir;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
+    public static void main(String[] args) {
+        List<Integer> nums =Arrays.asList(3,7,2,9,4,6,5,8);
+
+        nums.stream()
+                .map(n->n*n)
+                .forEach(System.out::println);
+
+        System.out.println();
+        System.out.println("Even numbers :");
+        nums.stream()
+                .filter(i->i%2==0)
+                .forEach(System.out::println);
+    }
 }
--- Collection/src/shrinivassir/StringClass.java
+++ Collection/src/shrinivassir/StringClass.java
@@ -1,4 +1,19 @@
 package shrinivassir;
 
-public class String {
+import java.util.Arrays;
+import java.util.List;
+
+public class StringClass {
+    public static void main(String[] args) {
+        List<String> strings= Arrays.asList("Go","Java","Python","Android","SQL");
+
+        strings.stream()
+                .map(l->l.length())
+                .forEach(System.out::println);
+
+        System.out.println();
+        strings.stream()
+                .filter(s->s.startsWith("A"))
+                .forEach(System.out::println);
+    }
 }
